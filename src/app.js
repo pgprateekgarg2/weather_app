a84c5451752c733db9da7f75ec98468a4ec8e3c3
Prateek Garg <pgprateekgarg2@gmail.com>
2020-01-28 17:00:54 +0530

    making changes for deployment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const weather = require('./utils/weather')
 
 
 const app  = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public') 
@@ -100,7 +101,7 @@ app.get('*',(req,res)=>{
 })
 
 // starting server
-app.listen(3000, ()=>{
-    console.log('server is up on port 3000')
+app.listen(port, ()=>{
+    console.log('server is up on port '+port)
 })
 
